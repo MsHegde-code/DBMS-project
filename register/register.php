3ec1ee7ba1de7e949467638e822ba40aacbe162e
Vinay <ksvinay06@gmail.com>
2023-01-21 00:18:51 +0530

    Revert "Update register.php"
    This reverts commit 4b88fdfd7f2012842758bede3b4ae8882d720d35.

--- register/register.php
+++ register/register.php
@@ -1,21 +1,19 @@
 <?php
-    //variables
-    $fname = $_POST['fname']
-    $lname $_POST['lname']
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $phone = $_POST['phone']
-    $city = $_POST['city']
-    $dob = $_POST['dob']
-    $gender = $_POST['gender']
-
+    $phone = $_POST['phone'];
+    $city = $_POST['city'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
 
     $conn = new mysqli('localhost','root','','dbms');
     if($conn->connect_error){
         die('connection failed :' . $conn->connect_error);
     }
     else{
-        $stmt = $conn->prepare("INSERT INTO registerform (fname, lname, email, password, date, gender, phone, city, date-in) VALUES (?,?,?,?,?,?,?,?, current_timestamp())")
+        $stmt = $conn->prepare("insert into registerform(fname, lname, email, password, dob, gender, phone, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
         $stmt->bind_param("ssssssis",$fname,$lname,$email,$password,$dob,$gender,$phone,$city);
         $stmt->execute();
         echo "successful";
