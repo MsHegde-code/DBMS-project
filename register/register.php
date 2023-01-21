3632b09ada33bf1620e9753ceabc7d9c3c9086cb
MSH <84580747+MsHegde-code@users.noreply.github.com>
2023-01-21 09:54:26 +0530

    php

--- register/register.php
+++ register/register.php
@@ -1,23 +1,13 @@
 <?php
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $phone = $_POST['phone'];
-    $city = $_POST['city'];
-    $dob = $_POST['dob'];
-    $gender = $_POST['gender'];
+    $server = "localhost";
+    $username = "root";
+    $password = "";
 
-    $conn = new mysqli('localhost','root','','dbms');
-    if($conn->connect_error){
-        die('connection failed :' . $conn->connect_error);
+    $con = mysqli_connect($server, $username, $password);
+
+    if(!$con){
+        die("connect failed" . mysqli_connect_error());
     }
-    else{
-        $stmt = $conn->prepare("insert into registerform(fname, lname, email, password, dob, gender, phone, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
-        $stmt->bind_param("ssssssis",$fname,$lname,$email,$password,$dob,$gender,$phone,$city);
-        $stmt->execute();
-        echo "successful";
-        $stmt->close();
-        $conn->close();
-    }
+
+    echo "connection successful";
 ?>
