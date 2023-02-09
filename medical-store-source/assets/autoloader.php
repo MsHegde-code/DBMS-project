d3d38dd38f2c4e1d3ed7d35b800f32fefc14e60b
MSH <84580747+MsHegde-code@users.noreply.github.com>
2023-02-09 11:43:02 +0530

    original project
    updating new project

--- medical-store-source/assets/autoloader.php
+++
@@ -1,6 +0,0 @@
-
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
-<script src='js/jquery.js'></script>
-<script src='js/bootstrap.min.js'></script>
-
--- medical-store-source/clear.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-$_SESSION['bill'] = array();
-$url = "location:".$_GET['url'];
-header($url);
-
- ?>
